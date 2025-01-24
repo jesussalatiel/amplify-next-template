549dbec4467acbdfbd9cc327ba9ffafc0576264b
Jesus Salatiel <jesus.bustamante@globant.com>
2025-01-24 00:07:12 -0600

    fix(ci): stablish connections

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        USER_POOL_CLIENT_ID: process.env.USER_POOL_CLIENT_ID,
+        USER_POOL_ID: process.env.USER_POOL_ID,
+        AWS_REGION: process.env.AWS_REGION,
+        IDENTITY_POOL_ID: process.env.IDENTITY_POOL_ID,
+        URL: process.env.URL,
+        API_KEY: process.env.API_KEY,
+      },
+}
 
 module.exports = nextConfig

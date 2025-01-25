2e9474630d8570b166783f3d1830e0ab952fa617
Jesus Salatiel <jesus.bustamante@globant.com>
2025-01-25 16:10:21 -0600

    fix(ci): added prettier

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  cacheHandler: require.resolve("./cache-handler.js"), // Ruta al manejador de caché
-  cacheMaxMemorySize: 50, // Tamaño máximo de la caché en MB
-  env: {
-    USER_POOL_CLIENT_ID: process.env.USER_POOL_CLIENT_ID,
-    USER_POOL_ID: process.env.USER_POOL_ID,
-    AWS_REGION: process.env.AWS_REGION,
-    IDENTITY_POOL_ID: process.env.IDENTITY_POOL_ID,
-    URL: process.env.URL,
-    API_KEY: process.env.API_KEY,
+  output: "export",
+  distDir: "build",
+  reactStrictMode: false,
+  images: {
+    unoptimized: true,
   },
 };
 
